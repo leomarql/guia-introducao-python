abd192c592b84aad4f432489085c80e2a5111814
leomarql <leo.marques03@hotmail.com>
2023-05-19 02:16:31 +0000

    #007 - Bibliotecas

--- Desafio 6/01_Modulos.py
+++ Desafio 6/01_Modulos.py
@@ -4,9 +4,11 @@ import sys
 
 # Printar o Sistema Operacional que voce esta usando:
 # YOUR CODE HERE
+print("Sistema operacional: ", sys.platform)
 
 # Printar a versão de Python que voce esta usando:
 # YOUR CODE HERE
+print("Versao do python: ", sys.version)
 
 
 import os
@@ -14,9 +16,12 @@ import os
 
 # Printar a process Id atual
 # YOUR CODE HERE
+print("Process ID: ", os.getpid())
 
 # Printar o atual diretório:
 # YOUR CODE HERE
+print("Atual diretorio: ", os.getcwd())
 
 # Printar o nome da maquina
 # YOUR CODE HERE
+print("Nome da maquina: ", os.uname().nodename)
--- Desafio 6/02_Numpy.py
+++ Desafio 6/02_Numpy.py
@@ -1,36 +1,58 @@
-
 import numpy as np
 
 #1- Crie uma matriz 1D com números de 0 a 9
-
+arr = np.arange(10) #CRIA UMA MATRIZ 1D COM 10 PARAMETROS, DE 0 A 9 
+print(arr)
 
 #2- Crie uma matriz booleana numpy 3×3 com ‘True’
-
+bool_arr = np.full((3, 3), True, dtype = bool)
+print(bool_arr)
 
 #3- Extraia todos os números ímpares de ‘arr’
 
 arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-#4- Substitua todos os números ímpares arr por -1
+num_impares = []
 
+for i in range(0, 10):
+    if arr[i]%2 != 0:
+        num_impares.append(arr[i])
+
+print(num_impares)
+
+#4- Substitua todos os números ímpares arr por -1
+for i in range(0, 10):
+    if i % 2 != 0:
+        arr[i] = -1
+
+print(arr)
 
 #5- Substitua todos os números ímpares em arr com -1 sem alterar arr
 
 arr = np.arange(10)
-#out = np.where()
+out = np.where(arr % 2 != 0, -1, arr)
+
+print(arr)
+print(out)
 
 #6- Converta uma matriz 1D para uma matriz 2D com 2 linhas:
 
 arr = np.arange(10)
+print(arr)
 
-       
+arr = arr.reshape(2, -1)
+print(arr)
+
 #6- Empilhe matrizes verticalmente:
 
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
-
+matriz_vertical = np.vstack((a, b))
+print(matriz_vertical)
 
 #6- Empilhe as matrizes horizontalmente:
 
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
+matriz_horizontal = np.hstack((a, b))
+print(matriz_horizontal)
